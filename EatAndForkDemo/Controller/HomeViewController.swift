7b99af85ea634a05f0d20c5cb1bc1a5214a765ee
Anurak Teerarattananukulchai <anurak.teera@gmail.com>
2023-09-18 23:05:05 +0700

    finished setting up HomePage UI and HomeViewController

---
+++ EatAndForkDemo/Controller/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  EatAndForkDemo
+//
+//  Created by anurak teerarattananukulchai on 18/9/2566 BE.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    //MARK: - IBOutlet
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: -IBAction
+    
+    @IBAction func listViewPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func gridViewPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func summaryViewTapped(_ sender: UITapGestureRecognizer) {
+        
+    }
+    
+  
+}
+
